2fbe3166ada8d7d054dab4abe50319b42640324c
jacksonjjones <jacksonjjones1@gmail.com>
2024-03-18 18:31:18 -0500

    Completed index and tag models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,10 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'productTag_products',
+  foreignKey: 'tag_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
 
